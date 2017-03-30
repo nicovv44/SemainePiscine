c881f40262874ffcd1750ecae205e7d3f083451b
Nicolas VERHELST <nico.v.44@gmail.com>
2017-03-30 18:38:14 +0200

    Ajout des fichiers de base et implémentation du footer avec compteur nombre visite en PHP

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8" />
+		<title>Projet piscine</title>
+		<link type="text/css" rel="stylesheet" href="style1.css">
+	</head>
+
+	<body>
+		<header>
+			<!-- contenu -->
+			<nav>
+				<!-- contenu -->
+			</nav>
+		</header>
+		
+		<!-- contenu -->
+		
+		<footer>
+			Hébergé par </br>
+			&copy; 2017 Mathidle Bridron <a href="mailto:mathilde.bridron@edu.ece.fr">mathilde.bridron@edu.ece.fr</a>, Nicolas VERHELST <a href="mailto:nicolas.verhelst@edu.ece.fr">nicolas.verhelst@edu.ece.fr</a><br/>
+			Nombre de visites : 
+			<?php
+				$filename = 'nbrVisites.txt';
+				$nombreDeVisites = null;
+
+				//on recupère le nombre de visite dans le fichier
+				if (is_readable($filename)) {
+					if (!$handle = fopen($filename, 'r')) {//ouverture en mode lecture
+						 echo "Impossible d'ouvrir le fichier ($filename)";
+						 exit;
+					}
+					if (($nombreDeVisites = fread($handle, 100)) === FALSE) {//on lit jusqu'à 100 caractères
+						echo "Impossible de lire le fichier ($filename)";
+						exit;
+					}
+					fclose($handle);
+				} else {
+					echo "Le fichier $filename n'est pas accessible en lecture ou est introuvable.";
+				}
+				
+				//on affiche le nombre de visites
+				if($nombreDeVisites) {echo $nombreDeVisites;}
+				
+				//on incrémente le nombre de visite récupéré dans le fichier
+				if($nombreDeVisites != null) {$nombreDeVisites++;}
+				
+				//on écrit ce nouveau nombre dans le fichier
+				if (is_writable($filename)) {
+					if (!$handle = fopen($filename, 'w')) {//ouverture en mode écriture
+						 echo "Impossible d'ouvrir le fichier ($filename)";
+						 exit;
+					}
+					if (fwrite($handle, $nombreDeVisites) === FALSE) {//on écrit le nouveau nombre par dessus l'encien
+						echo "Impossible d'écrire dans le fichier ($filename)";
+						exit;
+					}
+					fclose($handle);
+				} else {
+					echo "Le fichier $filename n'est pas accessible en écriture ou est introuvable.";
+				}
+			?>
+		</footer>
+		
+		<script src="JS1.js"></script>
+	</body>
+</html> 
